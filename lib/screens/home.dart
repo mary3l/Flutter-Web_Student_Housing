1eb2d9960b4eb23342d61187b9d79fcb50161dfd
seanchvz <seann.chvz@gmail.com>
2024-04-24 23:30:15 +0800

    Ui for dashboard

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_finals_web/screens/items.dart';
 import 'package:flutter_finals_web/providers/auth_provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -29,6 +32,7 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         child: Card(
+          color: Colors.indigo[900],
           margin: EdgeInsets.all(20),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(3),
@@ -43,8 +47,10 @@ class HomeScreen extends StatelessWidget {
                     padding: EdgeInsets.all(8.0),
                     child: Text(
                       'Welcome, $userName!',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
@@ -61,7 +67,74 @@ class HomeScreen extends StatelessWidget {
                             onPressed: () {
                               _signOut(context);
                             },
-                            child: Text('Sign Out'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors
+                                  .orange, // Set the background color to orange
+                            ),
+                            child: Text(
+                              'Sign Out',
+                              style: TextStyle(
+                                color: Colors
+                                    .black, // Set the text color to orange
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Houses with Dorm rooms',
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ItemScreen()),
+                            );
+                          },
+                          child: Container(
+                            width: MediaQuery.of(context).size.width *
+                                0.3, // 80% of screen width
+                            height: MediaQuery.of(context).size.height *
+                                0.4, // 40% of screen height
+                            child: Card(
+                              color: Colors.grey[900],
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              child: Column(
+                                children: [
+                                  Image.asset(
+                                    'assets/images/bg.jpg', // Replace with your image asset
+                                    width: 150,
+                                    height: 150,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      'Your Title',
+                                      style: TextStyle(
+                                        color: Colors.orange,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         ),
                       ),
